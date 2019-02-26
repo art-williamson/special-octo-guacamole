6000fe5ee8b6178e5b777de363254b8eb14e3659
Art Williamson <alwilliamson@xactware.com>
2019-02-25 20:56:53 -0700

    Added xcode project

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Art Williamson on 2/25/19.
+//  Copyright © 2019 Art Williamson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
