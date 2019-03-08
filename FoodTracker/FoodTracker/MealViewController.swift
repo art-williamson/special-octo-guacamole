8019c9f52ee94ba02fa2fb11e2188fc1e79ba006
Art Williamson <alwilliamson@xactware.com>
2019-03-07 21:22:28 -0700

    added ability to edit an existing cell as well as delete existing cells

--- FoodTracker/FoodTracker/MealViewController.swift
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -32,6 +32,14 @@ class MealViewController: UIViewController, UITextFieldDelegate,
         //handle the text field's user input through deleagates
         nameTextField.delegate = self
 
+        // Set up views if editing an existing Meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         //enable thesave button only if there is text in the text field
         updateSaveButtonState()
     }
@@ -106,7 +114,6 @@ class MealViewController: UIViewController, UITextFieldDelegate,
             return
         }
 
-
         let name = nameTextField.text ?? ""
         let photo = photoImageView.image
         let rating = ratingControl.rating
@@ -116,7 +123,22 @@ class MealViewController: UIViewController, UITextFieldDelegate,
     }
 
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+
+        // Depending on style of presentation (modal or push presentation),
+        //this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+
+        else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }
+
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
 }
 
